157c755a8839382b627f403d60e1b36b81ec32bb
T34Virus <treindle@gmail.com>
2023-02-20 15:58:04 -0800

    Updates

--- src/types/CardTypes.tsx
+++
@@ -1,9 +0,0 @@
-export interface CardType {
-    [key: string]: {
-        name: string;
-        image_path: string;
-        description: string;
-        upright: Array<string>;
-        reverse: Array<string>;
-    } 
-}
